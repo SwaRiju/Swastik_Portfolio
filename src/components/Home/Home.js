cee4de976a6106a92ecb8e42c7584f87fe3ec25b
SwaRiju <swastik200203bey@gmail.com>
2026-08-03 02:01:37 +0530

    Resume Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -61,7 +61,7 @@ function Home() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/SwaRiju"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
