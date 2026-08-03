6888929ef97f035cfda402152186b265f89e991a
SwaRiju <swastik200203bey@gmail.com>
2026-08-03 21:04:07 +0530

    Project card fixed

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,32 +7,32 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+      <Card.Body className="project-card-body">
+          <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <div className="project-card-contents">
+          <Card.Title><strong className="purple">{props.title}</strong></Card.Title>
+          <Card.Text style={{ textAlign: "justify", fontSize: "15px" }}>
+            {props.description}
+          </Card.Text>
+        </div>
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        <div className="project-card-buttons">
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp; GitHub
           </Button>
-        )}
+
+          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+          {props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+            >
+              <CgWebsite /> &nbsp; Demo
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
